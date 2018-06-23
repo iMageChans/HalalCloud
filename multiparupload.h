eefe88d877250310f1ea9e3b5284e66606a090e6
Chans <imagechans@gmail.com>
2018-06-23 19:01:19 +0800

    Create Loginout

--- multiparupload.h
+++ multiparupload.h
@@ -14,16 +14,19 @@ signals:
 public slots:
 
 private:
+    void getUploadToken(QString url, QString token, QString key);
+    void defaultConfig(QString path, QString token, QString uploadbatch);
+
     QString Token;
-    QString key;
+    QString Key;
     QString Url;
-    QString path;
-    QString iploadBatch;
+    QString Path;
+    QString UploadBatch;
     int Size;
     int Progress;
-    int block_size;
-    int bput_size;
-    int blocknum;
+    int Block_size;
+    int Bput_size;
+    int Blocknum;
 };
 
 #endif // MULTIPARUPLOAD_H
